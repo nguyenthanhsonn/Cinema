a6daaa245b215ec904a871ec7b7eb4d7108297fe
Son Nguyen <xelon1929@gmail.com>
2026-04-27 20:27:39 +0700

    refactor: remove redundant Roles decorator declaration

--- src/auth/decorators/roles.decorator.ts
+++ src/auth/decorators/roles.decorator.ts
@@ -6,5 +6,3 @@ export const ROLES_KEY = 'roles';
 export const Roles = (...roles: UserRole[]) => SetMetadata(ROLES_KEY, roles);
 
 
-export const Roles = (...roles: UserRole[]) => SetMetadata(ROLES_KEY, roles);
-
